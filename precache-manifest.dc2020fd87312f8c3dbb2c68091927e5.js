7a6957de086d86229f3dbbada3bc62e9c256ea1b
ambigudus (from Travis CI) <545660146@qq.com>
2020-12-05 19:24:58 +0000

    Deploy ambigudus/covid19-china-React to github.com/ambigudus/covid19-china-React.git:gh-pages

---
+++ precache-manifest.dc2020fd87312f8c3dbb2c68091927e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a47b0e77cc2e1e4175f0e370c79627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "decf78368607c01879a8",
+    "url": "/static/css/main.980bf927.chunk.css"
+  },
+  {
+    "revision": "57390265667e88d91672",
+    "url": "/static/js/2.1e552692.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1e552692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "decf78368607c01879a8",
+    "url": "/static/js/main.07b2bfae.chunk.js"
+  },
+  {
+    "revision": "07ce2dae1816e323c953",
+    "url": "/static/js/runtime-main.8708953b.js"
+  }
+]);
